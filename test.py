a9878d732e0cb34711a2ef8a9ed06ec65f42bf81
TonyBimenyi <83509726+TonyBimenyi@users.noreply.github.com>
2024-05-30 14:36:48 +0800

    recent changes

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 # Step Factor Initializations
 rho = 0.25
 eta = 1
-lamda = 0.5
+lamda = 1
 mu = 2.5
 epsilon = 10**(-5)
 
@@ -20,12 +20,13 @@ u2 = np.zeros((400, 1))
 u3 = np.zeros((400, 1))
 u4 = np.zeros((400, 1))
 
+#Error initializations
 e1 = np.zeros((401, 1))
 e2 = np.zeros((401, 1))
 e3 = np.zeros((401, 1))
 e4 = np.zeros((401, 1))
 
-# Definition of yd and y as arrays
+# Definition of yd, si and y as arrays
 y1 = np.zeros((401, 1))
 y2 = np.zeros((401, 1))
 y3 = np.zeros((401, 1))
@@ -88,20 +89,43 @@ for k in range(400):
     y3[1] = 3.5
     y4[1] = 4
     y1[k+1] = (y1[k] * u1[k]) / (1 + y1[k]**2) + u1[k]
-    y2[k+1] = (y2[k] * u2[k]) / (1 + y2[k]**2) + u2[k]
-    y3[k+1] = (y3[k] * u3[k]) / (1 + y3[k]**2) + u3[k]
-    y4[k+1] = (y4[k] * u4[k]) / (1 + y4[k]**2) + u4[k]
+    y2[k+1] = (y2[k] * u2[k]) / (1 + y2[k]**3) + 0.5 * u2[k]
+    y3[k+1] = (y3[k] * u3[k]) / (1 + y3[k]**2) + 0.9 * u3[k]
+    y4[k+1] = (y4[k] * u4[k]) / (1 + y4[k]**5) + 0.8 * u4[k]
 
     e1[k] = yd[k] - y1[k]
     e2[k] = yd[k] - y2[k]
     e3[k] = yd[k] - y3[k]
     e4[k] = yd[k] - y4[k]
 
+#Create_subplots
+
+fig, axs = plt.subplots(2, 2, figsize=(10, 8))
+fig1 = axs[0,0]
+fig2 = axs[0,1]
+fig3 = axs[1,0]
+
+fig1.plot(yd, '-b', label='Desired Output')
+fig1.plot(y1[:-1], '-r', label='y1')
+fig1.plot(y2[:-1], '-g', label='y2')
+fig1.plot(y3[:-1], '-y', label='y3')
+fig1.plot(y4[:-1], '-k', label='y4')
+fig1.grid(True)
+
+# fig2.plot(yd, '-b', label='Desired Output')
+fig2.plot(e1[:-1], '-r', label='y1')
+fig2.plot(e2[:-1], '-g', label='y2')
+fig2.plot(e3[:-1], '-y', label='y3')
+fig2.plot(e4[:-1], '-k', label='y4')
+fig2.grid(True)
+
 # Plot the desired output vs the actual output
-plt.plot(yd, '-b', label='Desired Output')
-plt.plot(y1[:-1], '-r', label='y1')
-plt.plot(y2[:-1], '-g', label='y2')
-plt.plot(y3[:-1], '-y', label='y3')
-plt.plot(y4[:-1], '-k', label='y4')
-plt.legend()
+# plt.plot(yd, '-b', label='Desired Output')
+# plt.plot(y1[:-1], '-r', label='y1')
+# plt.plot(y2[:-1], '-g', label='y2')
+# plt.plot(y3[:-1], '-y', label='y3')
+# plt.plot(y4[:-1], '-k', label='y4')
+
+# plt.legend()
+# plt.grid(True)
 plt.show()
